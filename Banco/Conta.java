d69bacb4682e10510a881b91d5933c3c9e5bee14
Andrey Cesar <andreycesar02@gmail.com>
2023-10-10 22:18:47 -0300

    Polimorfismo

---
+++ Banco/Conta.java
@@ -0,0 +1,13 @@
+public class Conta{
+    public int numConta;
+    protected int tipo;
+    private String dono;
+    private double saldo;
+    private boolean status;
+    public void abrirConta();
+    public void fecharConta();
+    public void depositar();
+    public void sacar();
+    public void pagarMensal();
+
+}
