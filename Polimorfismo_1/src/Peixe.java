ae8de06cfa2b5d758408a215bfb496ae60af91c0
Andrey <andrey.22204708360042@faeterj-rio.edu.br>
2023-10-24 16:58:58 -0300

    Polimorfismo

---
+++ Polimorfismo_1/src/Peixe.java
@@ -0,0 +1,20 @@
+public class Peixe extends Animal{
+    private String corEscama;
+    public void soltarBolha(){
+
+    }
+    @Override
+    public void locomover() {
+
+    }
+
+    @Override
+    public void alimentar() {
+
+    }
+
+    @Override
+    public void emitirSom() {
+
+    }
+}
