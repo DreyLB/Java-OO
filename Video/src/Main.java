314dcc88ed38cf0cecdee962aae2ce28b284bfab
Andrey <andreycesar02@gmail.com>
2023-10-28 00:58:56 -0300

    Projeto Final Curso em Video

--- Video/src/Main.java
+++ Video/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Video[] v = new Video[3];
+
+        v[0] = new Video("Aula 1 de POO");
+        v[1] = new Video("Aula 12 de PHP");
+        v[2] = new Video("Aula 10 de HTML5");
+
+        Usuario[] g = new Usuario[2];
+        g[0] = new Usuario("Nericson", 22, "Masculino", "Neloca");
+        g[1] = new Usuario("Merlinda", 23, "Feminino", "Mel");
+
+        Visualizacao vis = new Visualizacao(g[0], v[2]);
+        vis.avaliar(76.0);
+        System.out.println(vis.toString());
+
     }
 }
---
+++ Video/src/Usuario.java
@@ -0,0 +1,42 @@
+public class Usuario extends Pessoa{
+    private String login;
+    private int totAssistido;
+
+    public Usuario(String nome, int idade, String sexo, String login) {
+        super(nome, idade, sexo);
+        setLogin(login);
+        setTotAssistido(0);
+    }
+
+    public void viuMaisUm(){
+        this.totAssistido++;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotAssistido() {
+        return totAssistido;
+    }
+
+    public void setTotAssistido(int totAssistido) {
+        this.totAssistido = totAssistido;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuário{" +
+                "login='" + login + '\'' +
+                ", totAssistido=" + totAssistido +
+                ", nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", sexo='" + sexo + '\'' +
+                ", experiencia=" + experiencia +
+                '}';
+    }
+}
